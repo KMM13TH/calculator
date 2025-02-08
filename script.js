3f99c099a71570794d74d32e7056989881b509d2
sandhyakamath <sandhya_kammath@yahoo.co.in>
2025-02-08 18:53:26 +0000

    calculartor activity completed

---
+++ script.js
@@ -0,0 +1,7 @@
+// Access all the elements in the calculartor
+let historyValue = document.getElementById("history-value");
+let outputValue = document.getElementById("output-value");
+
+let clearButton = document.getElementById("clear");
+let backspaceButton = document.getElementById("backspace");
+let equalsButton = documents.getElementById("=");
